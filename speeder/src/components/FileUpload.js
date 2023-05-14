b7cddc0853f6dfcf7afff5d29b17a652c7664b8f
Rithvik <vikturi99@utexas.edu>
2023-05-14 17:39:46 -0500

    project react

---
+++ speeder/src/components/FileUpload.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import AWS from 'aws-sdk';
+
+class FileUpload extends Component {
+    state = {
+        selectedFile: null
+    };    
+    handleUpload = () => {
+        alert("File uploaded")
+    };
+    onFileUpload = () => {
+        // Create an object of formData
+        const formData = new FormData();
+       
+        // Update the formData object
+        formData.append(
+          "myFile",
+          this.state.selectedFile,
+          this.state.selectedFile.name
+        );
+       
+        // Details of the uploaded file
+        console.log(this.state.selectedFile);
+       
+        // Request made to the backend api
+        // Send formData object
+        //axios.post("api/uploadfile", formData);
+        
+    };
+    onFileChange = event => {    
+        // Update the state
+        this.setState({ selectedFile: event.target.files[0] });    
+    };
+    render() {
+        return (
+            <div className="App">
+                <form>
+                  <input type="file" onChange={this.onFileChange} />
+                  <button onClick={this.onFileUpload} type="submit">Upload</button>
+                </form>
+            </div>
+          );
+    }
+    
+  }
+export default FileUpload;
