c67a066b3451c04f188c443a36f801ce2e5073dd
Rithvik Vellaturi <vikturi99@gmail.com>
2023-06-01 00:14:06 -0500

    Added start/stop and css design

--- speeder/src/App.js
+++ speeder/src/App.js
@@ -1,11 +1,124 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import FileUpload from './components/FileUpload';
-import TextUpload from './components/TextUpload';
+
 function App() {
+  const [text, setText] = useState('');
+  const [speed, setSpeed] = useState(60);
+  const [words, setWords] = useState([]);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [isFlashing, setIsFlashing] = useState(false);
+  const restartRef = useRef(null);
+
+  useEffect(() => {
+    const splitText = text.split(' ');
+    setWords(splitText);
+    setCurrentWordIndex(0);
+  }, [text]);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isFlashing) {
+      const delay = (60 / speed) * 1000; // Calculate the delay in milliseconds
+      interval = setInterval(() => {
+        setCurrentWordIndex((prevIndex) => {
+          const nextIndex = prevIndex + 1;
+          if (nextIndex >= words.length) {
+            setIsFlashing(false);
+            return prevIndex;
+          }
+          return nextIndex;
+        });
+      }, delay);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isFlashing, words, speed]);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSpeedChange = (event) => {
+    setSpeed(Number(event.target.value));
+  };
+
+  const handleStartFlashing = () => {
+    setIsFlashing(true);
+  };
+
+  const handleStopFlashing = () => {
+    setIsFlashing(false);
+  };
+
+  const handleRestartFlashing = () => {
+    setCurrentWordIndex(0);
+    setIsFlashing(true);
+    restartRef.current.blur();
+  };
+
   return (
-    <div className="App">      
-      <TextUpload></TextUpload>
+    <div className="app-container">
+      <h1 className="app-title">Text Flasher</h1>
+      <div className="flasher-container">
+        <div className={`flasher ${isFlashing ? 'flash' : ''}`}>
+          {words[currentWordIndex]}
+        </div>
+      </div>
+      <div className="controls-container">
+        <div className="input-group">
+          <label htmlFor="speed" className="label">
+            Flash speed (wpm):
+          </label>
+          <input
+            type="number"
+            id="speed"
+            className="input"
+            min="1"
+            value={speed}
+            onChange={handleSpeedChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="text" className="label">
+            Enter text:
+          </label>
+          <input
+            type="text"
+            id="text"
+            className="input"
+            value={text}
+            onChange={handleTextChange}
+          />
+        </div>
+        <div className="button-group">
+          {isFlashing ? (
+            <button className="button" onClick={handleStopFlashing}>
+              Stop Flashing
+            </button>
+          ) : (
+            <button
+              className="button"
+              onClick={handleStartFlashing}
+              disabled={text === ''}
+            >
+              Start Flashing
+            </button>
+          )}
+          <button
+            className="button"
+            onClick={handleRestartFlashing}
+            ref={restartRef}
+            disabled={!isFlashing && currentWordIndex === 0}
+          >
+            Restart
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
